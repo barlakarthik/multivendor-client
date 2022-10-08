e57db44f76b640001d0892264eba9fc374d77032
karthikyadav <karthik.b@risecorp.com>
2022-10-08 15:11:13 +0530

    change

--- multi-vendor-frontend/src/App.js
+++ multi-vendor-frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import Caleculator from './Components/ReactTasks/Caleculator';
+// import Caleculator from './Components/ReactTasks/Calculator/Caleculator';
 import Dynamic from './Components/ReactTasks/DynamicChangingNumbers/Dynamic';
 
 function App() {
