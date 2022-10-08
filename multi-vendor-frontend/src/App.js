537664b8cd15cb7d23be748d2ee8035ce419e452
karthikyadav <karthik.b@risecorp.com>
2022-10-08 15:09:26 +0530

    dynamic

--- multi-vendor-frontend/src/App.js
+++ multi-vendor-frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import Caleculator from './Components/ReactTasks/Caleculator';
+// import Caleculator from './Components/ReactTasks/Caleculator';
+import Dynamic from './Components/ReactTasks/DynamicChangingNumbers/Dynamic';
 
 function App() {
   return (
     <div className="App">
-      <Caleculator />
+      {/* <Caleculator /> */}
+      <Dynamic />
     </div>
   );
 }
---
+++ multi-vendor-frontend/src/Components/ReactTasks/DynamicChangingNumbers/Dynamic.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import './Dynamic.css';
+const Dynamic = () => {
+    const [number, setNumber] = useState(0)
+    return (
+        <div className='container'>
+            <div className='card'>
+                {number}
+            </div>&nbsp;
+            <div>
+                <button type="button" className='btn btn-outline-success' onClick={() => setNumber(Num => Num + 1)}>Increament</button>&nbsp;
+                <button type="button" class="btn btn-outline-danger" onClick={() => setNumber(Num => Num == 0 ? Num : Num - 1)}>Decrement</button>
+            </div>
+        </div>
+    )
+}
+
+export default Dynamic
