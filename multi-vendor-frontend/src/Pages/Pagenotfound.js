12ca97ed2561ed461dd7c0ad872add06ebc55373
karthikyadav <karthik.b@risecorp.com>
2022-10-19 15:53:21 +0530

    chvx

--- multi-vendor-frontend/src/Pages/Pagenotfound.js
+++ multi-vendor-frontend/src/Pages/Pagenotfound.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Pagenotfound = () => {
     return (
-        <h1>Pagenotfound</h1>
+        <>
+            <h1>Pagenotfound</h1>
+            <img src='https://banner2.cleanpng.com/20180618/zoy/kisspng-decision-making-stock-photography-person-concept-duvida-5b2757672216d4.9148906515293049351396.jpg' />
+        </>
     )
 }
 
